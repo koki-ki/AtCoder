0dea4e7016e9d28b652de566217601831da74c9a
ck-ksst <0822koki@gmail.com>
2022-10-29 23:28:07 +0900

    main

---
+++ ABC/275/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using vs = vector<string>;
+using Graph = vector<vector<ll>>;
+
+const ll MOD = 1000000007;
+const ll INF = 2000000000;
+/* define short */
+#define pb push_back
+#define mp make_pair
+
+/* repeat macro */
+#define rep(i, n) for (ll i = 0; i < (ll)n; i++)
+#define rep2(i, s, n) for (ll i = s; i < (ll)n; i++)
+
+int main() {
+    ll n;
+    cin >> n;
+    vl h(n);
+    rep(i, n) cin >> h[i];
+    ll ans = 0;
+    rep(i, n) if (h[ans] < h[i]) ans = i;
+    cout << ans + 1 << endl;
+}
