9aec0bb7a29a7fd14ac5c12e8ebc7c513275f66d
ck-ksst <0822koki@gmail.com>
2023-06-05 19:15:55 +0900

    master

--- ABC/286/C.py
+++ ABC/286/C.py
@@ -1,2 +1,16 @@
+INF = 1 << 60
+
 n, a, b = map(int, input().split())
-s = input()
+s = input() * 2
+
+ans = INF
+
+for i in range(n):
+    tmp = a * i
+    for j in range(n // 2):
+        if s[i + j] != s[i + n - 1 - j]:
+            tmp += b
+
+    ans = min(ans, tmp)
+
+print(ans)
